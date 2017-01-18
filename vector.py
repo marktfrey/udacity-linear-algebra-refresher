8c612229590e6bad613e2a7abc76aa7602ef5733
Mark Frey <mfrey@thinkthroughmath.com>
2017-01-18 07:09:26 -0500

    Add vector addition, vector subtraction, and scalar multiplication
    overloaded magic methods, but this may prove to be a bad idea.
    not checking for type or vector length.

--- vector.py
+++ vector.py
@@ -16,6 +16,17 @@ class Vector(object):
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
-
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def __add__(self, v):
+        return Vector([a + b for a, b in zip(self.coordinates, v.coordinates)])
+
+    def __sub__(self, v):
+        return Vector([a - b for a, b in zip(self.coordinates, v.coordinates)])
+
+    def __mul__(self, i):
+        return Vector([a * i for a in self.coordinates])
+
+    def __rmul__(self, i):
+        return Vector([a * i for a in self.coordinates])
